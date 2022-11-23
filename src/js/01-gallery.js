ca74d2c5ea9614f1d002285cdbee00ff7846db6d
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-11-23 22:16:07 +0800

    task 1 Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// Створення та рендеринг розмітки по масиву даних galleryItems 
+// та представленому шаблону galleryItem
+
+const galleryItemMarkup = creatGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function creatGalleryMarkup(galleryItems) {
+return galleryItems
+    .map(({ preview, original, description }) => {
+
+// Ініціалізація бібліотеки SimpleLightbox
+    return `<li>
+        <a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </li>`;
+    })
+    .join("");
+}
+
+// з допомогою пункту Options додаємо підпис на картинку та швидкість анімацїї
+const lightbox = new SimpleLightbox(".gallery__item", {
+captionsData: "alt",
+captionDelay: 250,
+});
